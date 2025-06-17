54f2fe3b926c90da10027de3e54c183c1e501a31
hawyeet <53439203+knz04@users.noreply.github.com>
2025-06-17 20:31:24 +0700

    add custom todo input

--- cmd/ToGoList/main.go
+++ cmd/ToGoList/main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"bufio"
+	"errors"
 	"flag"
-	"os"
 	"fmt"
-	
+	"io"
+	"os"
+	"strings"
+
 	todo "github.com/knz04/ToGoList"
 )
 
@@ -29,8 +33,13 @@ func main() {
 
 	switch {
 	case *add:
-		todos.Add("Sample todo")
-		err := todos.Store(todoFile)
+		task, err := getInput(os.Stdin, flag.Args()...)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err.Error())
+			os.Exit(1)
+		}
+		todos.Add(task)
+		err = todos.Store(todoFile)
 		if err != nil {
 			fmt.Fprintln(os.Stderr, err.Error())
 			os.Exit(1)
@@ -63,4 +72,22 @@ func main() {
 		fmt.Fprintln(os.Stdout, "invalid command")
 		os.Exit(0)
 	}
+}
+
+func getInput(r io.Reader, args ...string) (string, error)  {
+	if len(args) > 0 {
+		return strings.Join(args, " "), nil
+	}
+
+	scanner := bufio.NewScanner(r)
+	scanner.Scan()
+	if err := scanner.Err(); err != nil {
+		return "", err
+	}
+
+	if len(scanner.Text()) == 0 {
+		return "", errors.New("empty todo is not allowed")
+	}
+
+	return scanner.Text(), nil
 }
